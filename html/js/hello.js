7398bdd680156f37b64f0dec694621bd6c5b9c86
cheskt <andy9793@naver.com>
2025-04-03 15:47:42 +0900

    js

---
+++ html/js/hello.js
@@ -0,0 +1,13 @@
+console.log('Hello world');
+
+const myName = 'Sico22';
+
+function printName(nm){
+    console.log(1, nm);
+
+    const myName = 'hana'
+    console.log(2, myName);
+}
+
+printName(myName);
+console.log(3, myName);
