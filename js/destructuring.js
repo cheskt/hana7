d74e0db4ca2e289575b72b3aad6174d4129420e0
cheskt <andy9793@naver.com>
2025-04-08 16:21:46 +0900

    strict

--- js/destructuring.js
+++ js/destructuring.js
@@ -1,3 +1,4 @@
+'use strict'
 // const u = {id: 1, name: 'Hong', age: 29};   // object
 // let {id, name, addr} = u; // let id = 1 ⇐ let id = user.id;  let addr = undefined;
 // let {id, ...info} = u;
