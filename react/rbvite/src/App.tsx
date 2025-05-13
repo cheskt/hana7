f313c42af1137f563ce7d0e312ad2406659844e6
cheskt <andy9793@naver.com>
2025-05-13 17:18:37 +0900

    loginhandler

--- react/rbvite/src/App.tsx
+++ react/rbvite/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
-import Hello from "./components/Hello";
+import Hello, { type HelloHandler } from "./components/Hello";
 import My from "./components/My";
+import type { LoginHandler } from "./components/Login";
 
 export type LoginUser = {
   id: number;
@@ -34,10 +35,19 @@ const SampleSession: Session = {
 function App() {
   const [session, setSession] = useState<Session>(SampleSession);
   const [count, setCount] = useState(0);
+  const helloButtonRef = useRef<HTMLButtonElement>(null);
+  const logoutButtonRef = useRef<HTMLButtonElement>(null);
+  const helloHandlerRef = useRef<HelloHandler>(null);
+
+  const loginHandlerRef = useRef<LoginHandler>(null);
 
   const plusCount = () => setCount((c) => c + 1);
   const login = (id: number, name: string) => {
-    setSession({ ...session, loginUser: { id, name } });
+    if (!loginHandlerRef.current) return;
+    const { getName, validate, str, focusId } = loginHandlerRef.current;
+    console.log("login>>>>", getName(), str);
+    if (validate()) setSession({ ...session, loginUser: { id, name } });
+    else focusId();
   };
 
   const logout = () => {
@@ -54,6 +64,7 @@ function App() {
 
   const addItem = (name: string, price: number) => {
     const id = Math.max(...session.cart.map((item) => item.id), 0) + 1;
+    console.log("🚀 name:", id, name, price);
     setSession({ ...session, cart: [...session.cart, { id, name, price }] });
   };
 
@@ -76,10 +87,27 @@ function App() {
         removeItem={removeItem}
         addItem={addItem}
         editItem={editItem}
+        logoutButtonRef={logoutButtonRef}
+        loginHandlerRef={loginHandlerRef}
       />
-      <Hello name={"홍길동"} age={33} plusCount={plusCount}>
+      <Hello
+        name={"홍길동"}
+        age={33}
+        plusCount={plusCount}
+        helloButtonRef={helloButtonRef}
+        refx={helloHandlerRef}
+      >
         반갑습니다!
       </Hello>
+      <button onClick={() => helloButtonRef.current?.click()}>
+        Click Hello
+      </button>
+      <button onClick={() => logoutButtonRef.current?.click()}>
+        Logout in App
+      </button>
+      <button onClick={() => console.log(helloHandlerRef.current)}>
+        sayHello
+      </button>
     </>
   );
 }
