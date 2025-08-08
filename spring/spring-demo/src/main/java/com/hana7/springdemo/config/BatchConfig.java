d21c075f95eb2fe473cd1d239eec0b595212cae6
cheskt <andy9793@naver.com>
2025-08-08 16:59:50 +0900

    scheduling

---
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/config/BatchConfig.java
@@ -0,0 +1,21 @@
+package com.hana7.springdemo.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchConfig {
+	@Bean
+	public Job csvJob(JobRepository jobRepository, Step step) {
+		return new JobBuilder("csvJob", jobRepository)
+			.incrementer(new RunIdIncrementer())
+			.start(step)
+			.build();
+	}
+}
+
--- spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/entity/Memo.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/entity/Memo.java
@@ -1,11 +1,17 @@
 package com.hana7.springdemo.jpa.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -15,22 +21,27 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "memo")
+@DynamicInsert
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@Builder //객체를 생성할 수 있게 처리함,
-//@AllArgsConstructor와 @NoArgsConstructor를 같이 처리해야 함
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class Memo extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int mno;
 
-	@Column(length = 200, nullable = false)
+	@Column(nullable = false, length = 200)
 	private String memoText;
+
+	@Enumerated(EnumType.STRING)
+	@ColumnDefault("'PAYED'")
+	private MemoState state;
+
+	@Column(columnDefinition
+		= "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime statedAt;
 }
-
-
